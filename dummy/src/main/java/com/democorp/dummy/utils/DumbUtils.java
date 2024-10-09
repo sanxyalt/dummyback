78ed4423cdcfa5b1990bcd0d85beb2840a00b630
sanxyalt <trwmaios@gmail.com>
2024-10-09 12:10:44 +0300

    added null check

--- dummy/src/main/java/com/democorp/dummy/utils/DumbUtils.java
+++ dummy/src/main/java/com/democorp/dummy/utils/DumbUtils.java
@@ -4,7 +4,10 @@ import com.democorp.dummy.data.dto.enums.DumbTitle;
 
 public class DumbUtils {
 
-    public static String determineTitleFromRank(Integer dumbRank){
+    public static String determineTitleFromRank(Integer dumbRank) {
+        if (dumbRank == null) {
+            return DumbTitle.PLAIN_DUMB.getValue();
+        }
         return switch (dumbRank) {
             case 1 -> DumbTitle.STUPIDITY_EMPEROR.getValue();
             case 2 -> DumbTitle.KING_OF_THE_STUPIDS.getValue();
