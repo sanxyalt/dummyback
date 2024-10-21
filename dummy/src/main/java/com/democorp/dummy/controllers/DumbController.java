f74cd6846751e871e4487b102e0c0af53af64cf8
sanxyalt <trwmaios@gmail.com>
2024-10-21 20:04:55 +0300

    added logging

--- dummy/src/main/java/com/democorp/dummy/controllers/DumbController.java
+++ dummy/src/main/java/com/democorp/dummy/controllers/DumbController.java
@@ -3,6 +3,8 @@ package com.democorp.dummy.controllers;
 import com.democorp.dummy.data.dto.ShitRequestDto;
 import com.democorp.dummy.data.dto.ShitResponseDto;
 import com.democorp.dummy.services.DumbService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,6 +15,8 @@ import java.net.URI;
 @RequestMapping("/dumb")
 public class DumbController {
 
+    private static final Logger logger = LogManager.getLogger(DumbController.class);
+
     private final DumbService dumbService;
 
     public DumbController(DumbService dumbService) {
@@ -21,12 +25,15 @@ public class DumbController {
 
     @GetMapping("/{id}")
     ResponseEntity<ShitResponseDto> fetchDumbShiet(@PathVariable("id") Integer id) {
+        logger.info("Fetching dumb shit with id : {}", id);
         ShitResponseDto response = dumbService.fetchShit(id);
         return ResponseEntity.ok().body(response);
     }
 
     @PostMapping
     ResponseEntity<ShitResponseDto> createNewDumbShit(@RequestBody ShitRequestDto request) {
+        logger.info("Creating a new dumb shit.");
+        logger.debug("The create request : {}", request);
         ShitResponseDto response = dumbService.createUpdateShit(null, request);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -39,6 +46,7 @@ public class DumbController {
     @PutMapping("/{id}")
     ResponseEntity<ShitResponseDto> updateDumbShit(@PathVariable("id") Integer id,
                                                    @RequestBody ShitRequestDto request) {
+        logger.info("Updating dumb shit with id : {}", id);
         ShitResponseDto response = dumbService.createUpdateShit(id, request);
         return ResponseEntity.ok().body(response);
     }
